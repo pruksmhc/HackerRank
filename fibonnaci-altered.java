4cc2a06fa2d5a09a45be55e500443a53304a6dbf
Yada Pruksachatkun <yadacmis@gmail.com>
2015-08-30 11:23:47 +0700

    Solved Fibonacci modified

---
+++ fibonnaci-altered.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+import java.lang.Math; 
+
+public class Solution {
+
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+         Scanner scan= new Scanner(System.in); 
+         BigInteger inputZero = scan.nextBigInteger(); 
+         BigInteger inputOne = scan.nextBigInteger();   
+         int numToFound = scan.nextInt();    
+         System.out.println(fibonnacirecurs(numToFound, inputZero, inputOne));       
+    }
+
+ 
+
+    private static BigInteger fibonaccirecurs(int num, BigInteger inputZero, BigInteger inputOne){
+     if (num == 1){
+    	return inputZero; 
+    	} 
+    	else if(num == 2){
+    	return inputOne; 
+    	}else{ //recursive case. 
+    		//the mathematical formula. 
+    		return (fibonaccirecurs(num-1, inputZero, inputOne)).multiply(fibonaccirecurs(num-1, inputZero, inputOne)).add(fibonaccirecurs(num-2, inputZero, inputOne));  //this should return the fibonnaci sequence. 
+
+    	}
+    }
+
+
+}
