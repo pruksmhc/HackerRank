cbf1e02883fe8a1135bb2fa2085919f38cac4a5a
Yada Pruksachatkun <pruks22y@mtholyoke.edu>
2016-08-25 19:47:48 -0400

    Update Manasa_and_Stones.java

--- Algorithm/Manasa_and_Stones.java
+++ Algorithm/Manasa_and_Stones.java
@@ -6,31 +6,26 @@
 //while the current value of the nth stone is less than the max, add 
 //the difference to the sum. 
 
-
 import java.util.*;
 public class Solution {
        
   public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int numExperiments = in.nextInt(); 
-        for(int i = 0; i < numExperiments; i++){
-        int numStones = in.nextInt()-1;
-        int a = in.nextInt(); 
-           
-        int b = in.nextInt();  
-        int biggerDifference = Math.max(a, b); 
-        int smallerDifference = Math.min(a,b); 
-        Integer currentValue = (Integer) smallerDifference * numStones; 
-        Integer maxValue = (Integer) biggerDifference * numStones; 
-        int difference  = biggerDifference - smallerDifference; 
-        String valueOfStone = currentValue.toString();
-         	while(currentValue < maxValue){
-         		currentValue+= difference; 
-         		valueOfStone+=" "+ currentValue.toString(); 
-
-            }
-         	System.out.println(valueOfStone); 
-         }
+        for (int i = 0; i < numExperiments; i++) {
+               int numStones = in.nextInt()-1;
+               int a = in.nextInt(); 
+               int b = in.nextInt();  
+               int biggerDifference = Math.max(a, b); 
+               int smallerDifference = Math.min(a,b); 
+               Integer currentValue = (Integer) smallerDifference * numStones; 
+               Integer maxValue = (Integer) biggerDifference * numStones; 
+               int difference  = biggerDifference - smallerDifference; 
+               String valueOfStone = currentValue.toString();
+               while(currentValue < maxValue){
+         	       currentValue+= difference; 
+         	       valueOfStone+=" "+ currentValue.toString(); 
+               }
         }
-      
-    }
+  }
+}
