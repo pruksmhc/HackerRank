75add93fb11964056940f4425a2c95760439d462
Yada Pruksachatkun <pruks22y@mtholyoke.edu>
2016-08-25 20:05:26 -0400

    Update World_Cup_Championships_Part_1.java

--- World_Cup_Championships_Part_1.java
+++ World_Cup_Championships_Part_1.java
@@ -5,12 +5,12 @@ public class Solution {
         Scanner in = new Scanner(System.in);
         //find, for the first coach, the first, third, and fifth highest, and then add them  together. 	
         ArrayList<Integer> numPoints = new ArrayList<Integer>(); 
-        for(int i =0; i < 10;i++){
-        	numPoints.add(in.nextInt());  //adding the numberpoints ot the arraylist. 
+        for (int i =0; i < 10; i++) {
+              numPoints.add(in.nextInt());  //adding the numberpoints ot the arraylist. 
         }
         Collections.sort(numPoints); //sort the arraylist 
-      Collections.reverse(numPoints); 
-        int highestPoints =0; 
+        Collections.reverse(numPoints); 
+        int highestPoints = 0; 
         highestPoints += numPoints.get(0); 
         highestPoints += numPoints.get(2); 
         highestPoints += numPoints.get(4); 
